01a891d0ed4cb0f74b7e3097e07e6db5054ced16
JamshedK <47409663+JamshedK@users.noreply.github.com>
2026-08-14 10:12:33 -0500

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # Goal: backtracking, we pass two numbers, num_open, num_closed
+        # base case: if num_open == num_closed == n, we stop add paranthesis and move one
+        # recursive case: 
+            # if num_open == num_closed, we can only add open
+            # elif num_open == n, we can only add closed
+            # otherwise, we can add both open and closed
+        res = []
+        def dfs(curr, num_open, num_closed):
+            if num_open == num_closed == n: 
+                res.append(curr)
+                return 
+            elif num_open == num_closed: 
+                dfs(curr + '(', num_open + 1, num_closed)
+            elif num_open == n:
+                dfs(curr + ')', num_open, num_closed + 1)
+            else: 
+                dfs(curr + '(', num_open + 1, num_closed)
+                dfs(curr + ')', num_open, num_closed + 1)
+        
+        dfs('', 0, 0)
+        return res
+
