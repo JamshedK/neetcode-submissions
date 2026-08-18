aacfcfc7b0b96a69db5ed0792cfa0ce0570f6468
JamshedK <47409663+JamshedK@users.noreply.github.com>
2026-08-18 11:21:57 -0500

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # Goal: Detect that there are no cycles and all the nodes are connected
+        
+        adj_list = [[] for i in range(n)]
+        for a, b in edges: 
+            adj_list[a].append(b)
+            adj_list[b].append(a)
+        par = [i for i in range(n)]
+        rank = [1] * n
+
+        def find(n1):
+            p = n1
+            while par[n1] != n1: 
+                n1 = par[n1] 
+            return n1
+        
+        def union(n1, n2):
+            p1, p2 =  find(n1), find(n2)
+            # if parents are the same, skip 
+            if p1 == p2: 
+                return False
+            # otherwise, connect them using whichever has a greater rank
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else: 
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+        for n1, n2 in edges:
+            if union(n1, n2) == False: 
+                return False
+        
+        return len(edges) == n - 1
