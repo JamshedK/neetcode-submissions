63b75b99060dd3f4fee44f23d21e3b606a68f22f
JamshedK <47409663+JamshedK@users.noreply.github.com>
2026-08-19 11:36:41 -0500

    Add: counting-bits - submission-3

---
+++ Data Structures & Algorithms/counting-bits/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        if n < 1:
+            return [0]
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        for i in range(2, n + 1): 
+            temp = i >> 1
+            # if number if odd
+            if i & 1 == 1:
+                # print(f"i = {i} and shift right {i >> 1}")
+                dp[i] = dp[1] + dp[temp]
+            # if number is even
+            else:
+                dp[i] = dp[temp]
+        
+        return dp
