b87cdee55b7f96380dde4d9878b183bb7db78d94
JamshedK <47409663+JamshedK@users.noreply.github.com>
2026-08-23 15:06:07 -0500

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # recursion
+        # dfs(n) returns x ^ n  
+        # recurrence if number is odd, dfs(n) = dfs(n / 2) * dfs(n / 2)
+        # if number is even dfs(1) * dfs(n / 2) * dfs(n / 2)
+        dp = {1: x, 0: 1 }
+        def dfs(n):
+            if n in dp:
+                return dp[n]
+            # if even
+            if n % 2 == 0:
+                dp[n] = dfs(n // 2) * dfs (n // 2)
+            else:
+                dp[n] = dfs(1) * dfs(n // 2) * dfs(n // 2)
+            return dp[n]
+        res = dfs(abs(n))
+        if n < 0: 
+            return 1 / res
+        return res
