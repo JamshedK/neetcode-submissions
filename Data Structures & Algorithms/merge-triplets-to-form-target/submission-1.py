1a1363c24c8ef72498c453de227290eb2ad448a2
JamshedK <47409663+JamshedK@users.noreply.github.com>
2026-09-11 18:13:18 -0500

    Add: merge-triplets-to-form-target - submission-1

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        matched = set()
+
+        for a, b, c in triplets:
+            if a > target[0] or b > target[1] or c > target[2]:
+                continue
+
+            if a == target[0]:
+                matched.add(0)
+            if b == target[1]:
+                matched.add(1)
+            if c == target[2]:
+                matched.add(2)
+
+        return len(matched) == 3
+
+        
